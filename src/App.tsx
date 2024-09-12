80a10dbda68f4f69e919c16b11dbc1f905142d3c
Alex666000 <bogdanov777000@mail.ru>
2024-09-12 21:26:43 +0300

    2 - end

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,57 @@
 import './App.css'
 import { Task, Todolist } from './Todolist'
+import { useState } from 'react'
+
+export type FilterValues = 'all' | 'completed' | 'active'
 
 const App = () => {
-  const tasks1 = [
+  let [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
     { id: 4, title: 'Redux', isDone: false },
-  ]
+  ])
+  // храним значение фильтра тут и его надо в дочке изменять поэтому changeFilterTodo будет принимать: value
+  const [filter, setFilter] = useState<FilterValues>('all')
 
-  const tasks2: Task[] = [
-    { id: 1, title: 'Hello world', isDone: true },
-    { id: 2, title: 'I am Happy', isDone: false },
-  ]
+  // функции меняющие данные (useState) создаем там где и данные и прокидываем вниз данные: колбеки и параметры их
+  const removeTask = (taskId: number) => {
+    // отфильтрованный новый массив tasks
+    let filteredTasks = tasks.filter((t) => t.id !== taskId)
+    // всё что бы не делали всегда "сетаем" - чтобы данные изменились и разметка подстроилась под данные (произошла автоматически "перерисовка")
+    setTasks(filteredTasks)
+  }
+
+  // функции меняющие данные (useState) создаем там где и данные
+  const changeFilterTodo = (value: FilterValues) => {
+    setFilter(value)
+  }
+
+  let tasksForTodolist = tasks
+
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter((t) => t.isDone)
+  }
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter((t) => !t.isDone)
+  }
 
   return (
     <div className="App">
       <div className="App">
-        <Todolist title="What to learn" tasks={tasks1} />
-        <Todolist title="Songs" tasks={tasks2} />
+        <Todolist
+          title="What to learn"
+          tasks={tasksForTodolist}
+          onRemoveTaskClick={removeTask}
+          onChangeFilterClick={changeFilterTodo}
+        />
       </div>
     </div>
   )
 }
 
 export default App
+// делаем разметку - делаем данные - делаем разметку зависимоиот этих данных чтобы меняя данные разметка тоже менялась
+// меняем данные руками сначала "инишлстеит" видим что разметка реагирует потом меняем не руками
+// а при нажатии на кнопки
+
+// Итог видео 2: 1.19.00 мин
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,3 +1,5 @@
+import { FilterValues } from './App'
+
 export type Task = {
   id: number
   title: string
@@ -7,20 +9,38 @@ export type Task = {
 type Props = {
   title: string
   tasks: Task[]
+  onRemoveTaskClick: (taskId: number) => void
+  onChangeFilterClick: (value: FilterValues) => void
 }
 
-export const Todolist = ({ title, tasks }: Props) => {
+export const Todolist = ({
+  title,
+  tasks,
+  onRemoveTaskClick,
+  onChangeFilterClick,
+  ...rest
+}: Props) => {
+  const handleRemoveTask = (taskId: number) => onRemoveTaskClick(taskId)
+
+  const changeTodoFilter = (filter: FilterValues) => onChangeFilterClick(filter)
+
   return (
-    <div>
+    <div {...rest}>
       <h3>{title}</h3>
       <ul>
         {tasks.map((t) => (
           <li>
             <input type="checkbox" checked={t.isDone} />
             <span>{t.title}</span>
+            <button onClick={() => handleRemoveTask(t.id)}>X</button>
           </li>
         ))}
       </ul>
+      <div>
+        <button onClick={() => changeTodoFilter('all')}>All</button>
+        <button onClick={() => changeTodoFilter('active')}>Active</button>
+        <button onClick={() => changeTodoFilter('completed')}>Completed</button>
+      </div>
     </div>
   )
 }
